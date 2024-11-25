db8fe7bfbafde9e0a046b9151320216b40a0113d
Vlad <160039489+55kt@users.noreply.github.com>
2024-11-25 19:01:27 +0200

    First Commit
    Hike Icon

---
+++ Hike/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Hike
+//
+//  Created by Vlad on 25/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image("image-1")
+            .resizable()
+            .scaledToFit()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Vlad on 25/11/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
