c6d59c961bc5c0d1212916cb4d856a9b37ca9384
Vlad <160039489+55kt@users.noreply.github.com>
2024-11-26 17:51:44 +0200

    Custom Button Style

--- Hike/Component/CardView.swift
+++ Hike/Component/CardView.swift
@@ -8,6 +8,28 @@
 import SwiftUI
 
 struct CardView: View {
+    // MARK: - Properties
+    @State private var imageNumber: Int = 1
+    @State private var randomNumber: Int = 1
+    
+    // MARK: - Functions
+    
+    func randomImage() {
+        print("--- THE BUTTON WAS TAPPED ---")
+        print("Status: Old image number = \(imageNumber)")
+        
+        repeat {
+            randomNumber = Int.random(in: 1...5)
+            print("Action: Random number generated = \(randomNumber)")
+        } while randomNumber == imageNumber
+        
+        imageNumber = randomNumber
+        
+        print("Result: New image number = \(imageNumber)")
+        print("--- THE END ---")
+    }
+    
+    
     var body: some View {
         // MARK: - CARD
         ZStack {
@@ -52,11 +74,27 @@ struct CardView: View {
                         )
                         .frame(width: 256, height: 256)
                     
-                    Image("image-1")
+                    Image("image-\(imageNumber)")
                         .resizable()
                         .scaledToFit()
+                        .animation(.default, value: imageNumber)
                 }
                 // MARK: - FOOTER
+                
+                Button {
+                    // Generate a random number
+
+                    randomImage()
+                } label: {
+                    Text("Explore More")
+                        .font(.title2)
+                        .fontWeight(.heavy)
+                        .foregroundStyle(
+                            LinearGradient(colors: [.customGreenLight, .customGreenMedium], startPoint: .top, endPoint: .bottom)
+                        )
+                        .shadow(color: .black.opacity(0.25), radius: 0.25, x: 1, y: 2)
+                }
+                .buttonStyle(GradientButton())
             } //: VSTACK
         } //: CARD
         .frame(width: 320, height: 570)
